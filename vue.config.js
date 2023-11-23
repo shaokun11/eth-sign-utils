8fffacb2b0b92391d7ffca633d98f11857bc0889
shaokun <605416017@qq.com>
2023-11-23 18:23:42 +0800

    add demo

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+    transpileDependencies: true,
+    configureWebpack: {
+        plugins: [
+            new webpack.ProvidePlugin({
+                Buffer: ["buffer", "Buffer"],
+                process: ["process/browser"],
+            }),
+        ],
+        resolve: {
+            fallback: {
+                url: false,
+                util: false,
+                https: false,
+                zlib: false,
+                http: false,
+                process: require.resolve("process/browser"),
+                stream: require.resolve("stream-browserify"),
+            },
+        },
+    },
+});
